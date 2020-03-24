dfdf5c41f7357e5fd23d20fd5b124279a9122cce
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-03-24 22:56:52 +0530

    local and global variables using same name

--- chapter3/local_global.py
+++ chapter3/local_global.py
@@ -1,12 +1,15 @@
 def spam():
     eggs = 99
     bacon()
-    print(eggs)
+    print(bacon())
+    print("Eggs=",eggs)
 
 def bacon():
     ham = 101
     eggs = 0
+    return eggs
 
 spam()
 
-#Here only the spam function which is called will give value of eggs as it is the only local variable present inside the function
+# Here only the spam function which is called will give value
+# of eggs as it is the only local variable present inside the function
