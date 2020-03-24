e71a25606ce6bb68bcc331ed52db1eaa7148418d
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-03-24 11:51:53 +0530

    executed basic mathematical operations

---
+++ chapter1/mathematical_operations.py
@@ -0,0 +1,7 @@
+print(2**3)  #exponent i.e 2 raised to 3
+print(2+2)   #addition
+print(22%8)  #remainder
+print(22//8) #Integer division/floored quotient
+print(22/8)  #division
+print(3*5)   #multiplication
+print(5-2)   #Subtraction
