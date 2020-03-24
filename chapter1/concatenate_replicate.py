304ce7908f82512495ad1a1871ad3cc06363cc42
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-03-24 12:33:16 +0530

    concatenate and replicate

---
+++ chapter1/concatenate_replicate.py
@@ -0,0 +1,7 @@
+print("bob"+"alice")      #concatenation
+print("alice"*5)          #replication
+
+
+#You can't add a string and a number i.e "alice"+4
+#You can't multiply a string and a string i.e "alice"*"bob"
+#You can't multiply a string with a float i.e "alice"*5.0
