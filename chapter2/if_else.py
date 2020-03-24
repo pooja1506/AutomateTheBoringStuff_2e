180cef95963f1489bbce3116217fda144b51b5d1
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-03-24 14:35:07 +0530

    used if else statement

---
+++ chapter2/if_else.py
@@ -0,0 +1,10 @@
+print("enter a name ")
+name = input()
+print("enter a password")
+password = input()
+if name == "Mary":
+    print("Hello , Mary ")
+if password=="Mary123":
+    print("access granted")
+else:
+    print("access failed")
