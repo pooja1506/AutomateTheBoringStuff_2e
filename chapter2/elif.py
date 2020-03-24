ade2a9641270564934c36f958a5b44b0bfce1741
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-03-24 15:07:25 +0530

    else if statement

---
+++ chapter2/elif.py
@@ -0,0 +1,10 @@
+print("Enter your name")
+name= input()
+print("Enter your age")
+age= input()
+if name == 'Pooja':
+    print("Hello,Pooja")
+elif int(age)<23:
+    print("you are still a kiddo")
+else:
+    print("out of loop")
