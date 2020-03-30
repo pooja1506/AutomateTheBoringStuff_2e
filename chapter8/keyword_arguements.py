18e03a827a2fd3fc4d32b875ca20b8629587ca09
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-03-30 13:11:52 +0530

    min,max,greater than , less than

---
+++ chapter8/keyword_arguements.py
@@ -0,0 +1,4 @@
+import pyinputplus as pyip
+response = pyip.inputNum("Enter number:" , min=4)
+response1 = pyip.inputNum("Enter number:" , greaterThan=6)
+response2 = pyip.inputNum("Enter number:" , lessThan=5)
