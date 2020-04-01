ba4a87fc39014f90d4836a2c5734d6966e262cca
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-01 17:08:47 +0530

    try and except in requests

---
+++ chapter12/tryexcept.py
@@ -0,0 +1,6 @@
+import requests
+res = requests.get('https://inventwithpython.com/page_that_does_not_exist')
+try:
+    res.raise_for_status()
+except Exception as exc:
+    print('There was a problem: %s' % (exc))
