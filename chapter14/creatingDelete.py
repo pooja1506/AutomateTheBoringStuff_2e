327497b0a1103e9c6cb4845569fadfbcf060d0b9
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-02 22:03:18 +0530

    creating and deleting spreadsheets

--- chapter14/creatingDelete.py
+++ chapter14/creatingDelete.py
@@ -6,3 +6,14 @@ ss.createSheet('Eggs') # Create another new sheet.
 ss.sheetTitles
 ss.createSheet('Bacon', 0) code># Create a sheet at index 0 in the list of
 ss.sheetTitles
+
+ss.sheetTitles
+ss[0].delete()      # Delete the sheet at index 0: the "Bacon" sheet.
+ss.sheetTitles
+ss['Spam'].delete() # Delete the "Spam" sheet.
+ss.sheetTitles
+sheet = ss['Eggs']  # Assign a variable to the "Eggs" sheet.
+sheet.delete()      # Delete the "Eggs" sheet.
+ss.sheetTitles
+ss[0].clear()       # Clear all the cells on the "Sheet1" sheet.
+ss.sheetTitles      # The "Sheet1" sheet is empty but still exists.
