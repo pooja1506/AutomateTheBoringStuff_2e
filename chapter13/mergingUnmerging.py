4b8475dd1596b309339e5348d4682fe2bd1810f2
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-02 21:02:03 +0530

    merging and unmerging of cells

---
+++ chapter13/mergingUnmerging.py
@@ -0,0 +1,13 @@
+import openpyxl
+wb = openpyxl.Workbook()
+sheet = wb.active
+sheet.merge_cells('A1:D3') # Merge all these cells.
+sheet['A1'] = 'Twelve cells merged together.'
+sheet.merge_cells('C5:D5') # Merge these two cells.
+sheet['C5'] = 'Two merged cells.'
+wb.save('merged.xlsx')
+
+sheet.unmerge_cells('A1:D3') # Split these cells up.
+sheet.unmerge_cells('C5:D5')
+wb.save('merged.xlsx')
+
