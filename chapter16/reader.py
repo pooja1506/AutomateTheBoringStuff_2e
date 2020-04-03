d585ba5317d9eeb79e209d7e2bbda052174c18b7
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-03 18:26:44 +0530

    read and write csv

--- chapter16/reader.py
+++ chapter16/reader.py
@@ -8,3 +8,13 @@ exampleData
 #Loop
 for row in exampleReader:
     print('Row #' + str(exampleReader.line_num) + ' ' + str(row))
+
+
+
+ import csv
+outputFile = open('output.csv', 'w', newline='')
+outputWriter = csv.writer(outputFile)
+outputWriter.writerow(['spam', 'eggs', 'bacon', 'ham'])
+outputWriter.writerow(['Hello, world!', 'eggs', 'bacon', 'ham'])
+outputWriter.writerow([1, 2, 3.141592, 4])
+outputFile.close()
