206cc7f8e339ddedef8c8a9f16b906d1b781a0bb
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-03 18:32:45 +0530

    using dictionary writer to create a csv file

---
+++ chapter16/createCsv.py
@@ -0,0 +1,8 @@
+import csv
+outputFile = open('output.csv', 'w', newline='')
+outputDictWriter = csv.DictWriter(outputFile, ['Name', 'Pet', 'Phone'])
+outputDictWriter.writeheader()
+outputDictWriter.writerow({'Name': 'Alice', 'Pet': 'cat', 'Phone': '555-1234'})
+outputDictWriter.writerow({'Name': 'Bob', 'Phone': '555-9999'})
+outputDictWriter.writerow({'Phone': '555-5555', 'Name': 'Carol', 'Pet':'dog'})
+outputFile.close()
