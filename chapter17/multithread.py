b8161749166f28bc454046e2826072df2fa9b004
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-04 20:32:06 +0530

    Multithreading.py

--- chapter17/multithread.py
+++ chapter17/multithread.py
@@ -8,3 +8,8 @@ def takeANap():
 threadObj = threading.Thread(target=takeANap)
 threadObj.start()
 print('End of program.')
+
+
+
+threadObj = threading.Thread(target=print, args=['Cats', 'Dogs', 'Frogs'],kwargs={'sep': ' & '})
+threadObj.start()
