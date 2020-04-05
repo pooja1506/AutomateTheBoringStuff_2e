8c9668c2ca0598c2c077f2914c6807713b501fde
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-05 17:33:34 +0530

    resizing and rotating images

---
+++ chapter19/resizing.py
@@ -0,0 +1,16 @@
+#Resizing image
+from PIL import Image
+catIm = Image.open('zophie.png')
+width, height = catIm.size
+quartersizedIm = catIm.resize((int(width / 2), int(height / 2)))
+quartersizedIm.save('quartersized.png')
+svelteIm = catIm.resize((width, height + 300))
+svelteIm.save('svelte.png')
+
+#Rotating image
+
+from PIL import Image
+catIm = Image.open('zophie.png')
+catIm.rotate(90).save('rotated90.png')
+catIm.rotate(180).save('rotated180.png')
+catIm.rotate(270).save('rotated270.png')
