36a81654e5dbd2f50e890a087711f249627c2a5d
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-05 17:34:54 +0530

    resizing and rotating images

--- chapter19/resizing.py
+++ chapter19/resizing.py
@@ -14,3 +14,7 @@ catIm = Image.open('zophie.png')
 catIm.rotate(90).save('rotated90.png')
 catIm.rotate(180).save('rotated180.png')
 catIm.rotate(270).save('rotated270.png')
+
+#expanding of image
+catIm.rotate(6).save('rotated6.png')
+catIm.rotate(6, expand=True).save('rotated6_expanded.png')
